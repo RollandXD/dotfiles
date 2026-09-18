f4363003708d1eba5eeec554f4aff0259d370956
Rolland <15859826+RollandXD@user.noreply.gitee.com>
2026-09-19 01:15:30 +0800

    chore(yazi): 移除 git 状态插件
    去掉 git 插件的 setup 与 prepend_fetchers 配置。

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -2,10 +2,6 @@ require("full-border"):setup {
 	type = ui.Border.ROUNDED,
 }
 
-require("git"):setup {
-	order = 1500,
-}
-
 require("yamb"):setup {
 	bookmarks = {},  -- 留空：主打运行时 g a 打书签，不在此写死预设
 	jump_notify = true,
